4b6270def2f61c17ed00cc483c223cc642d5b641
krywa5 <blocker92@o2.pl>
2020-12-18 15:17:52 +0100

    add mail to author

--- src/components/Copyright/Copyright.jsx
+++ src/components/Copyright/Copyright.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
 
 import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/styles';
+
+const useStyles = makeStyles(theme => ({
+    link: {
+        textDecoration: 'none',
+        color: 'inherit',
+        transition: `color ${theme.transitions.easing.easeInOut} ${theme.transitions.duration.shorter}ms`,
+
+        "&:hover": {
+            color: theme.palette.secondary.main,
+        }
+    }
+}))
 
 const Copyright = () => {
+    const classes = useStyles();
+
     return (
         <Typography variant="body2" color="textSecondary" align="center">
-            Copyright © <b>Krystian Wasilewski</b> {new Date().getFullYear()}.
+            Copyright © <a href="mailto:k.wasilewski92@gmail.com" target='_blank' rel='noopener noreferrer' className={classes.link}><strong>Krystian Wasilewski</strong></a> {new Date().getFullYear()}
         </Typography>
     );
 }
