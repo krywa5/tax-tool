8b0b770ab9aaa983d930077c8c194f5ffcc80ab2
krywa5 <blocker92@o2.pl>
2021-01-04 22:46:23 +0100

    change cors proxy

--- src/data/fetch/currency.fetch.js
+++ src/data/fetch/currency.fetch.js
@@ -1,6 +1,6 @@
 export const currencyFetch = async (currencyValueDate = '', currency = 'EUR') => {
     const currencyFormatted = currency.toLowerCase();
-    const PROXY_URL = 'https://cors-anywhere.herokuapp.com/';
+    const PROXY_URL = 'https://thingproxy.freeboard.io/fetch/';
     const API_URL = `${PROXY_URL}http://api.nbp.pl/api/exchangerates/rates/a/${currencyFormatted}/${currencyValueDate}/?format=json`;
 
     const response = await fetch(API_URL);
